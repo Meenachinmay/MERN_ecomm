c80277e93671d588e61d39d7013b20e1f3d3b401
Chinmay anand <chinmayanand896@gmail.com>
2020-10-11 16:09:10 +0530

    rever

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Product from '../components/Product';
+
+const ProductScreen = () => {
+    return (
+        <h1>Product</h1>
+    )
+}
+
+export default ProductScreen;
